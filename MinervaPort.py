420f5f98715954c952fbd82afa576388ca92f209
michieldwitte <michieldwitte@gmail.com>
2012-12-03 00:54:55 +0100

    Update MinervaPort.py
    new version with "wxpython" and "requests" libraries

--- MinervaPort.py
+++ MinervaPort.py
@@ -1,129 +1,178 @@
+#!/usr/bin/python2
+
+
 from StringIO import StringIO
-import pycurl
+import requests
 import os
+import sys
+import wx
+import time
+from threading import Thread
 
-print "insert username:"
-username = str(raw_input())
-print "insert password:"
-password = str(raw_input())
-print username, ":", password
+cookie=""
+isThreadRunning=""
 
-#salt bemachtigen---------------------------------------
-c = pycurl.Curl()
-cookie = "tempcookies"
-c.setopt(pycurl.URL, "https://minerva.ugent.be/secure/index.php?external=true")
-c.setopt(pycurl.SSL_VERIFYPEER, False)
-c.setopt(pycurl.COOKIEFILE, cookie)
-storagebuffer = StringIO()
-c.setopt(pycurl.WRITEFUNCTION, storagebuffer.write)
-c.perform()
-c.close()
+def getSalt():
+    global cookie
+	r = requests.get('https://minerva.ugent.be/secure/index.php?external=true', verify=False)
+	lines = r.text.split("\n")
+	cookie = r.cookies
+	
+	for line in lines:
+	    if "authentication_salt" in line:
+			return line.split("\"")[5]
+
+def Login(username, password, salt):
+	global cookie
+	postdata = {'login' : str(username), 'password': str(password), 'authentication_salt': str(salt), 'submitAuth': 'Log in'}
+	r = requests.post("https://minerva.ugent.be/secure/index.php?external=true", data=postdata, cookies=cookie, verify=False)
+	cookie = r.cookies
+	if '<div id="CBINFO_ext" class="course course0 visible" >' in r.text:
+		return True
+	else:
+		return False
+
+def getCourses(courseid, coursename):
+	global cookie
+	r = requests.get("https://minerva.ugent.be/index.php", cookies=cookie, verify=False)
+	lines = r.text.split("\n")
+	cookie = r.cookies
+	
+	courses = []
+	for line in lines:
+	    if "course_home.php?cidReq=" in line:
+		if "class=\"course course0" not in line:
+		    courses.append(line)
+
+	tempcourse = ""
+
+	for i in courses:
+	    if "class=\"course course0" not in i:
+		temp = i.split("\"")[1]
+		if temp != "":
+		    courseid.append(temp)
+		    coursename.append(i.split("<")[5].split(">")[1])
+
+def recursiveCourseDownloader(line, coursename):
+	global cookie
+	extension = ""
+	if "http://minerva.ugent.be" in line:
+		line = line.replace("http://minerva.ugent.be","")
+ 	if "downloadfolder" in line:
+		extension = ".zip"
+	
+	url = str(line.split("\"")[15]).replace("amp;","")
+	filename = url.split("path=%2F")[1]
+
+	print "downloading: " + filename+extension
+	r = requests.get("http://minerva.ugent.be"+url , cookies=cookie, verify=False)
+	cookie = r.cookies
+	with open(coursename+"/"+filename+extension, "wb") as code:
+		code.write(r.content)
+
+def courseDownloader(courseid, coursename):
+	global cookie
+	if not os.path.exists(coursename):
+    		os.mkdir(coursename)
+	
+	r = requests.get("http://minerva.ugent.be/main/document/document.php?cidReq="+courseid, cookies=cookie, verify=False)
+	cookie = r.cookies
+	lines = r.text.split("\n")
+
+	for line in lines:
+		if "action=download" in line:
+			recursiveCourseDownloader(line, coursename)
+			if (not isThreadRunning):
+				return
 
 
-html = storagebuffer.getvalue()
-lines = html.split("\n")
-for line in lines:
-    if "authentication_salt" in line:
-        salt = line.split("\"")[5]
 
-#inloggen---------------------------------------
-  
-c = pycurl.Curl()
-c.setopt(pycurl.URL,"https://minerva.ugent.be/secure/index.php?external=true")
-c.setopt(pycurl.SSL_VERIFYPEER, False)
-c.setopt(pycurl.COOKIEJAR, cookie)
-c.setopt(pycurl.COOKIEFILE, cookie)
+def MinervaSyncer(username, password, button):
+	salt = getSalt()
+	returnvalue = Login(username,password,salt)
+	
+	if not returnvalue:
+		wx.MessageBox('Incorrect username and/or password', 'Error', wx.OK | wx.ICON_ERROR)
+		return
+	
+	courseid = []
+	coursename = []
+	getCourses(courseid, coursename) #dit nog recursief maken
+	
+	for i in range(0,courseid.__len__()):
+		courseDownloader(courseid[i],coursename[i])
+		if(not isThreadRunning):
+			return
+	
+	button.Enable()
+	return "Successfully synced with the directory!"
 
+class LoginDialog(wx.Frame):
+	def __init__(self):
+		wx.Frame.__init__(self, None, -1, "Minerva Syncer V0.1")
+		self.Bind(wx.EVT_CLOSE, self.OnClose)
+		self.InitUI()
+		self.SetSize((200,100))
+		self.Centre()
+		self.Fit()
+		self.Show()
+	
+	def InitUI(self):
+		hbox = wx.BoxSizer(wx.VERTICAL)
+		fgs = wx.FlexGridSizer(2, 2, hgap=5, vgap=10)
+		
+		self.txt_username = wx.TextCtrl(self, 1)
+		lbl_username = wx.StaticText(self,-1, "Username:")
+		fgs.Add(lbl_username)
+		fgs.Add(self.txt_username)
+		
+		self.txt_password = wx.TextCtrl(self, 1, style=wx.TE_PASSWORD)
+		lbl_password = wx.StaticText(self,-1, "Password:")
+		fgs.Add(lbl_password)
+		fgs.Add(self.txt_password)
+		
+		"""
+		fgs.Add(wx.StaticText(self,-1, label="Login:"))
+		fgs.Add(wx.TextCtrl(self))
+		fgs.Add(wx.StaticText(self,-1, label="Password:"))
+		fgs.Add(wx.TextCtrl(self, style=wx.TE_PASSWORD))
+		"""
 
-postdata = ""
-postdata += "login="
-postdata += username
-postdata += "&password="
-postdata += password
-postdata += "&authentication_salt="
-postdata += salt
-postdata += "&submitAuth=Log in"
+		hbox.Add(fgs)
+		self.SetSizer(hbox)
+		
+		self.button = wx.Button(self, -1, label="Sync")
+		self.Bind(wx.EVT_BUTTON, self.OnSubmit, self.button)
+		hbox.Add(self.button,0,wx.ALIGN_CENTER)
+		
+		#hbox.Add(wx.Button(self, label="Login"), 1, wx.ALIGN_CENTER)
+		
+	
+	def OnSubmit(self, event):
+		global isThreadRunning
+		username=self.txt_username.GetValue()
+		password=self.txt_password.GetValue()
+		if username != "" and password != "":
+			#print username + " | " + password
+			t = Thread(target=MinervaSyncer, args=(username,password,self.button,))
+			isThreadRunning=True
+			t.start()
+			self.button.Disable()
+			#MinervaSyncer(username, password)
+	
+	def OnClose(self, event):
+		global isThreadRunning
+		if(isThreadRunning):
+			isThreadRunning=False
+			self.Destroy()
+			#self.Destroy()
+			#sys.exit(1)
+		else:
+			self.Destroy()
+		
+		
+app = wx.App(False)
 
-c.setopt(pycurl.POST, 1)
-c.setopt(pycurl.POSTFIELDS, postdata)
+frame = LoginDialog()
 
-c.perform()
-c.close()
-
-#minerva site downloaden----------------------------------------------------------------------------
-#mss hier een wait van een seconde insteken ?
-c = pycurl.Curl()
-c.setopt(pycurl.URL,"https://minerva.ugent.be/index.php")
-c.setopt(pycurl.SSL_VERIFYPEER, False)
-c.setopt(pycurl.COOKIEJAR, cookie)
-c.setopt(pycurl.COOKIEFILE, cookie)
-storagebuffer.flush()
-c.setopt(pycurl.WRITEFUNCTION, storagebuffer.write)
-c.perform()
-c.close()
-
-courses = []
-html = storagebuffer.getvalue()
-lines = html.split("\n")
-for line in lines:
-    if "course_home.php?cidReq=" in line:
-        if "class=\"course course0" not in line:
-            courses.append(line)
-
-courseid = []
-coursename = []
-tempcourse = ""
-
-for i in courses:
-    if "class=\"course course0" not in i:
-        temp = i.split("\"")[1]
-        if temp != "":
-            courseid.append(temp)
-            coursename.append(i.split("<")[5].split(">")[1])
-
-#----------elk vak zijn documenthtml downloaden-------------------------------------------------------------
-for i in range(0,courseid.__len__()):
-    if not os.path.exists(coursename[i]):
-        os.mkdir(coursename[i])
-    c = pycurl.Curl()
-    url = "http://minerva.ugent.be/main/document/document.php?cidReq="
-    url += courseid[i]
-    c.setopt(pycurl.URL, url)
-    c.setopt(pycurl.SSL_VERIFYPEER, False)
-    c.setopt(pycurl.COOKIEJAR, cookie)
-    c.setopt(pycurl.COOKIEFILE, cookie)
-    storagebuffer.flush()
-    c.setopt(pycurl.WRITEFUNCTION, storagebuffer.write)
-    c.perform()
-    c.close()
-    html = ""
-    html = storagebuffer.getvalue()
-    
-    #-------------alle folders en files downloaden op die html
-    lines = []
-    lines = html.split("\n")
-    for line in lines:
-        if "action=download" in line:
-            extension = ""
-            if "http://minerva.ugent.be" in line:
-                line = line.replace("http://minerva.ugent.be","")
-            if "downloadfolder" in line:
-                extension = ".zip"
-            url = ""
-            filename = ""
-            url = str(line.split("\"")[15]).replace("amp;","")
-            filename = url.split("path=%2F")[1]
-            fp = open(coursename[i]+"/"+filename+extension, "wb")
-            
-            c = pycurl.Curl()
-            c.setopt(pycurl.URL, "http://minerva.ugent.be"+url)
-            c.setopt(pycurl.SSL_VERIFYPEER, False)
-            c.setopt(pycurl.COOKIEJAR, cookie)
-            c.setopt(pycurl.COOKIEFILE, cookie)
-            c.setopt(pycurl.WRITEDATA, fp)
-            c.perform()
-            c.close()
-            fp.close()
-            print filename +" is downloaded"
-
-print "DONE"
+app.MainLoop()
